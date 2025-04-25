6a028c83d11fddf9ea177e6d6151720134be4fb8
Meriem1551 <meriem200409@gmail.com>
2025-04-25 13:13:09 +0100

    initializing project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Main from './components/main/main'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/main/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main>
+            <h1>Welcome to Digitalia</h1>
+            <p>This is the main content of the website.</p>
+        </main>
+    );
+};
+
+export default Main;
